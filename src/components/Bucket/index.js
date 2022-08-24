7945d4329935bc5c835220c4ab677efe748aa076
axeljk <axeljkern@yahoo.com>
2022-08-24 14:09:39 -0400

    Removes default React starter page and adds a bucket component.

---
+++ src/components/Bucket/index.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+
+export default function Bucket() {
+	const [bucket, setBucket] = useState([]);
+
+	return (
+		<>
+			<h1>Hey there!</h1>
+			<p>This is placeholder text.</p>
+		</>
+	);
+}
